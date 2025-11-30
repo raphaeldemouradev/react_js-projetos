e0b3f9a9fa99d3f15976a1daacd6fb3036c087c2
Raphael de Moura <raphaelmoura.dev@gmail.com>
2025-11-30 12:49:22 -0300

    clima do dia atual concluido

--- projeto-clima/script.js
+++ projeto-clima/script.js
@@ -10,9 +10,13 @@ async function buscarCidade(cidade) {
 function colocaDadosNaTela(dados) {
     let resCidade = document.getElementById("res-cidade");
     let resGraus = document.getElementById("res-graus");
+    let resImg = document.getElementById("res-img")
+    let resTempo = document.getElementById("res-tempo");
 
     resCidade.innerHTML = "Tempo em " + dados.name + ":";
     resGraus.innerHTML = Number.parseInt(dados.main.temp) + "°C";
+    resImg.src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`;
+    resTempo.innerHTML = dados.weather[0].description;
 
     console.log("Completo!");
 }
@@ -23,3 +27,10 @@ function SearchButton() {
     buscarCidade(cidade);
     console.log("Procurando: " + cidade);
 }
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        const cidade = document.getElementById("input-cidade").value;
+        buscarCidade(cidade);
+    }
+});
