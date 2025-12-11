37a7c561eed4125b9c7802319119a8a3e7c271f0
Raphael de Moura <raphaelmoura.dev@gmail.com>
2025-12-10 23:31:16 -0300

    create catalogo de filme

---
+++ catalogo-filmes/catalogo-filmes/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Pagina de Inicio</h1>
+      <button><a href="#">Ir para Home</a></button>
+    </div>
+  )
+}
+
+export default App
---
+++ catalogo-filmes/catalogo-filmes/src/rotas/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <div>
+        <h2>Home pag</h2>
+      <button>
+        <a href="#">Voltar para Inicio</a>
+      </button>
+    </div>
+  )
+}
+
+export default Home
